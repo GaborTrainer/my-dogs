356d164e7c6f93013692598b5f4a4294114aa412
Gabor <szemelyiedzo29gmail.com>
2022-06-07 11:42:16 +0200

    prog tetelek

---
+++ public/landing.js
@@ -0,0 +1,39 @@
+function myScript() {
+  console.log('hello world');
+  console.log(eldontes());
+  console.log(kivalogatas());
+}
+function eldontes() {
+  const T = [1, 2, 3, 4, 5];
+  const e = 1;
+  for (let i = 0; i < T.length; i++) {
+    if (T[i] === e) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function kivalogatas() {
+  const T = [1, 2, 3, 4, 5];
+
+  const X = [];
+  let j = 0;
+
+  const Y = [];
+  let k = 0;
+
+  for (let i = 0; i < T.length; i++) {
+    if (T[i] % 2 === 0) {
+      X[j] = T[i];
+      j++;
+    } else {
+      Y[k] = T[i];
+      k++;
+    }
+  }
+  return {
+    X: X,
+    Y: Y,
+  };
+}
