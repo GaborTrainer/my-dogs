87806bc0b855106bba345df42a60b3938f2e2a98
Gabor <szemelyiedzo29gmail.com>
2022-06-09 10:32:02 +0200

    unio

--- public/landing.js
+++ public/landing.js
@@ -8,6 +8,8 @@ function myScript() {
   console.log(mimimumKivalasztas());
   console.log(metszetkepzes());
   console.log(kivalasztas());
+  console.log(linearisKereses());
+  console.log(unio());
 }
 
 function eldontes() {
@@ -105,8 +107,9 @@ function metszetkepzes() {
 
   for (let i = 0; i < t.length; i++) {
     j = 0;
-    while (j < o.length && o[j] != t[i])
+    while (j < o.length && o[j] != t[i]) {
       j++;
+    }
     if (j < o.length) {
       metszet[k++] = t[i];
     }
@@ -128,4 +131,58 @@ function kivalasztas() {
   return {
     x: x
   }
+}
+
+function linearisKereses() {
+  const t = [1, 4, 6, 8, 2];
+  const x = 6;
+
+  let i = 0;
+  while (t[i] != x && i < t.length) {
+    i++;
+  }
+  return {
+    exist: i < t.length,
+    index: i < t.length ? i : undefined
+  }
+}
+
+function unio() {
+  const a = [1, 2, 3];
+  let i = 0;
+
+  const b = [3, 4, 5];
+  let j = 0;
+
+  const z = [];
+  let k = 0;
+
+  let l = 0;
+
+  for (i = 0; i < a.length; i++) {
+    z[k] = a[i];
+    k++;
+  }
+  for (j = 0; j < b.length; j++) {
+
+
+    const x = b[j];
+
+    l = 0;
+    while (z[l] != x && l < z.length) {
+      l++;
+    }
+    if (l === z.length) {
+      z[k] = x;
+      k++;
+    }
+
+    /*if (z.indexOf(x) === -1) {
+      z.push(x);
+    }*/
+
+
+
+  }
+  return z;
 }
